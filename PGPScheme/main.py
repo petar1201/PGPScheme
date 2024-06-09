d8454268d941901c7107627dbd141524776e84e6
vuleticmina <minavuletic2001@gmail.com>
2024-06-09 16:21:14 +0200

    add triple_des

--- PGPScheme/main.py
+++ PGPScheme/main.py
@@ -1,8 +1,14 @@
-from PGPScheme.security.configuration import private_key_ring_collection
-
+from PGPScheme.security.configuration import *
+from message.message import Message
+from algorithms.triple_des import *
 
 private_key_ring_collection.add_key_pair("mina", "minavu@gmail.com", "mina123", 2048)
 
-from message.message import Message
+
 mess = Message("porukica", "naslov")
 mess.authentication("minavu@gmail.com|mina", "mina123")
+
+session_key = key_generator.generate_session_key(get_message_block())
+triple_des = TripleDes(session_key, "porukica")
+triple_des.encrypt()
+triple_des.decrypt()
